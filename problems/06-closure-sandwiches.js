81ff3221fde385af92556644ba8f02b1ef149e42
Keenly <keenlyhere@gmail.com>
2022-10-06 13:22:47 -0700

    06 07 answers

--- problems/06-closure-sandwiches.js
+++ problems/06-closure-sandwiches.js
@@ -18,6 +18,25 @@ sandwich2("pb") // => "One sandwich with tomato and pb"
 ***********************************************************************/
 
 // Your code here
+const sandwichMaker = () => {
+  let sandwich = "One sandwich with tomato";
+
+  return sandwichOrder = (ingredient) => {
+    sandwich = sandwich + " and " + ingredient;
+    return sandwich;
+  }
+
+}
+
+let sandwich = sandwichMaker(); // => returns a function
+sandwich("spinach") // => "One sandwich with tomato and spinach"
+sandwich("jelly") // => "One sandwich with tomato and spinach and jelly"
+sandwich("bread") // => "One sandwich with tomato and spinach and jelly and bread"
+console.log(sandwich("spinach"))
+console.log(sandwich("jelly"))
+console.log(sandwich("bread"))
+
+
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
